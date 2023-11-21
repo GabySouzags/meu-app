742bc82f2f24a783ef639dc4e4e8a1997df2a5b4
GabySouzags <gabrielasouzags01@gmail.com>
2023-11-21 19:55:03 -0300

    teste

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,25 +1,25 @@
-import './Letreiro.css';
+// import './Letreiro.css';
 
 
 
-const texto = "conheça a Fatec";
-let index = 0;
-let speed = 100;
+// const texto = "conheça a Fatec";
+// let index = 0;
+// let speed = 100;
 
-function escreverLetreiro() {
-  const letreiro = document.getElementById('letreiro');
+// function escreverLetreiro() {
+//   const letreiro = document.getElementById('letreiro');
   
-  if (index < texto.length) {
-    letreiro.innerHTML += texto.charAt(index);
-    index++;
-    setTimeout(escreverLetreiro, speed);
-  } else {
-    index = 0;
-    letreiro.innerHTML = '';
-    setTimeout(escreverLetreiro, speed);
-  }
-}
+//   if (index < texto.length) {
+//     letreiro.innerHTML += texto.charAt(index);
+//     index++;
+//     setTimeout(escreverLetreiro, speed);
+//   } else {
+//     index = 0;
+//     letreiro.innerHTML = '';
+//     setTimeout(escreverLetreiro, speed);
+//   }
+// }
 
-escreverLetreiro(); // Chama a função uma vez para iniciar o efeito
+// escreverLetreiro(); // Chama a função uma vez para iniciar o efeito
 
-export default escreverLetreiro; // Exporta a função para ser usada em outros arquivos, se necessário
+// export default escreverLetreiro; // Exporta a função para ser usada em outros arquivos, se necessário
