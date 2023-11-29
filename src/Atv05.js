d776450e70920d1bc11a19707a49353706d2283e
GabySouzags <gabrielasouzags01@gmail.com>
2023-11-28 21:48:35 -0300

    jogo da memoria

--- src/Atv05.js
+++ src/Atv05.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import JogoDaMemoria from "./JogoDaMemoria";
 
 
 export default function Atv05() {
     return (
         <>
             <h1>Atividade 05</h1>
-
+            <JogoDaMemoria/>
             <br />
             <Link to="/"> Retornar </Link>
         </>
