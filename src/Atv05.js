8e99281b46f3e382e3b6141e0aa4ec31b44ae0d6
GabySouzags <gabrielasouzags01@gmail.com>
2023-11-28 21:26:24 -0300

    calculadora

--- src/Atv05.js
+++ src/Atv05.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom"
+import Calculadora from "./Calculadora";
 
 export default function Atv05() {
     return (
         <>
             <h1>Atividade 05</h1>
+            <br />
+            <Calculadora/>
             <br />
             <Link to="/"> Retornar </Link>
         </>
